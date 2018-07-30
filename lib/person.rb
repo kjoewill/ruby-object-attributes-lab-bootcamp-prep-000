86c596eb0012521b1cb1a57efce6d1fb75b31aaa
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-30 04:52:38 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,19 @@
 class Person
   @name
+  @job
+  
+  def name= (n)
+    @name = n
+  end
+  def name
+    @name
+  end
+  
+  def job= (j)
+    @name = j
+  end
+  def job
+    @job
+  end
   
 end
